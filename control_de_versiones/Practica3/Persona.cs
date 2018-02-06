401f6956287fdcba01ae6e4e9e27e52521f32d5d
unknown <brandonqr1@gmail.com>
2018-02-06 16:25:33 +0100

    mi primer commit

---
+++ control_de_versiones/Practica3/Persona.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica3
+{
+    class Persona
+    {
+        //atributos
+        private string nom="";
+        private string dataNaixement="";
+        private string nif="";
+        private char sexe='H';
+        private double pes = 0;
+        private double alçada = 0;
+        #region MetodosPublicos
+        public string calcularIMC()
+        {
+            double resultat = pes / (Math.Pow(alçada,2));
+            string cadena = "";
+            if (resultat < 18)
+                cadena = "Baix";
+            if (resultat > 18 && resultat < 24.9)
+                cadena = "Normal";
+            else if (resultat > 25 && resultat < 26.9)
+                cadena = "Sobrepes";
+            else
+                cadena = "Obes";
+            return cadena;
+        }
+        public int edat() {
+            DateTime fecha = DateTime.Now;
+            int año = fecha.Year;
+            int AñoNacimiento = Int32.Parse(dataNaixement.Split('/')[2]);
+            return (año - AñoNacimiento);
+        }
+        public bool esMajorEdat() {
+            return (edat()>18)?true:false;
+        }
+        //metods publicos mios
+        public string entrarNif()
+        {
+            while (!nifCorrecte(nif))
+            {
+                Console.WriteLine("Entra Nif 11111111H: ");
+                nif = Console.ReadLine();
+                //nif = (nifCorrecte(nif)) ? nif : nif;
+            }
+            return nif;
+        }
+        public bool esHoD(char sexe)
+        {
+            bool trobat = false;
+            if (sexe.ToString().ToUpper() == "H" || sexe.ToString().ToUpper() == "D")
+            {
+                trobat = true;
+            }
+            return trobat;
+        }
+        private string entrarAño(string dataNaixement)
+        {
+            bool trobat = false;
+            DateTime fecha = DateTime.Now;
+            int añoActual = fecha.Year;
+            while (!trobat)
+            {
+                Console.WriteLine("Entra Data Naixement DD/MM/AAAA: ");
+                dataNaixement = Console.ReadLine();
+                string[] camposFecha = dataNaixement.Split('/');
+                try
+                {
+                    
+                    int dia = Int32.Parse(camposFecha[0]);
+                    int mes = Int32.Parse(camposFecha[1]);
+                    int año = Int32.Parse(camposFecha[2]);
+                    int diasMes = DateTime.DaysInMonth(año, mes);
+                    if (dia <= diasMes && mes < 13 && año <= añoActual)
+                    {
+                        trobat = true;
+                    }
+                }
+                catch (Exception) { }
+
+            }
+            return dataNaixement;
+        }
+        private  double introducirDouble(string cadena)
+        {
+            double numero = -1;
+            while (!(valorPositivo(numero)))
+            {
+                Console.WriteLine(cadena);
+                try { numero = Convert.ToDouble(Console.ReadLine()); }
+                catch (Exception) { }
+            }
+            return numero;
+        }
+        private static bool valorPositivo(double valor)
+        {
+            return (valor > 0) ? true : false;
+        }
+        #endregion
+        #region Propietats
+        public string Nom
+        {
+            get
+            {
+                return nom;
+            }
+
+            set
+            {
+                nom = value;
+            }
+        }
+
+        public string DataNaixement
+        {
+            get
+            {
+                return dataNaixement;
+            }
+
+            set
+            {
+                if (value == "")
+                    dataNaixement = entrarAño(value);
+                else
+                    dataNaixement = value;
+            }
+        }
+
+        public string Nif
+        {
+            get
+            {
+                return nif;
+            }
+
+            set
+            {
+                if (nifCorrecte(value))
+                    nif = value;
+                else
+                    entrarNif();
+            }
+        }
+
+        public char Sexe
+        {
+            get
+            {
+                return sexe;
+            }
+
+            set
+            {
+                sexe = esHoD(value) ? value : 'H';
+            }
+        }
+
+        public double Pes
+        {
+            get
+            {
+                return pes;
+            }
+
+            set
+            {
+                    pes = (value == 0)?introducirDouble("Entra Pes"): value;
+            }
+        }
+
+        public double Alçada
+        {
+            get
+            {
+                return alçada;
+            }
+
+            set
+            {
+                alçada = (value == 0) ? introducirDouble("Entra Altura") : value; ;
+            }
+        }
+        #endregion
+        #region Contructores
+        public Persona() {//constructor sin parametros
+        }
+        public Persona(string nif)
+        {
+            this.nif = (nifCorrecte(nif)) ? nif : "Nif Invalido";
+        }
+        public Persona(string nom, string dataNaixement, char sexe)
+        {
+            this.nom = nom;
+            this.dataNaixement = dataNaixement;
+            this.sexe = sexe;
+        }
+        public Persona(string nom, string dataNaixement, string nif, char sexe, double pes, double alçada) {
+            this.nom = nom;
+            this.dataNaixement = dataNaixement;
+            this.nif = (nifCorrecte(nif)) ? nif : "Nif Invalido";
+            this.sexe = sexe;
+            this.pes = pes;
+            this.alçada = alçada;
+        }
+        #endregion
+        #region metodes privats
+        private static bool nifCorrecte(string nif)
+        {
+            char lletra='.';
+            bool trobat = false;
+            int maxIndiceNif = nif.Length - 1;
+            int numero = 0;
+            try
+            {
+                numero = Int32.Parse(nif.Substring(0, maxIndiceNif));
+                lletra = nif[maxIndiceNif].ToString().ToUpper()[0];
+            }
+            catch (Exception) {}
+            return trobat = (lletraM(numero, lletra)) ? true : false; ;
+        }
+
+        static bool lletraM(int numero, char lletra)
+        {
+            string lletresDNI = "TRWAGMYFPDXBNJZSQVHLCKE";
+            return (lletra == lletresDNI[numero % 23]) ? true : false;
+        }
+        
+
+
+        #endregion
+        #region override
+        public override string ToString()
+        {
+            string cadena = "Datos de la Persona:" +
+                "\nNom: " + nom +
+                "\nData Naixement: " + dataNaixement +
+                "\nNif: " + nif +
+                "\nSexe: " + sexe +
+                "\npes: " + pes +
+                "\nalçada: " + alçada;
+
+            return cadena;
+        }
+        #endregion
+    }
+}
---
+++ control_de_versiones/Practica3/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona p = new Persona();
+            string nom="No Name", dataNaixement="", nif="";
+            char sexe='H';
+            double pes=0, alçada = 0;
+            Console.WriteLine("Entra Nom: ");
+            nom = Console.ReadLine();
+            p.DataNaixement = dataNaixement;
+            dataNaixement = p.DataNaixement;
+            //dataNaixement = entrarAño(dataNaixement);
+
+            p.Nif=nif;//set
+            nif = p.Nif;//get
+            Console.WriteLine("Entra Sexe: ");
+            try
+            {p.Sexe = Convert.ToChar(Console.ReadLine()); }catch (Exception) {p.Sexe='h';}
+
+            p.Pes = pes;
+            pes = p.Pes;
+           //introducirDouble("Entra Pes");
+
+
+            p.Alçada = alçada;  //introducirDouble("Entra Alçada");
+            alçada = p.Alçada;
+           
+            
+            menu(nom,dataNaixement,nif,sexe,pes,alçada);
+        }
+        private static void menu(string nom, string dataNaixement, string nif, char sexe, double pes, double alçada)
+        {
+            int opcion = 0;
+            do
+            {
+                Console.WriteLine("Elige una Opcion:\n1.Prova 1\n2.Prova 2\n3.Prova 3\n4.Salir");
+                try
+                {
+                    opcion = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception) { Console.WriteLine("Tipo de Valor no valido"); }
+                Console.Clear();
+                switch (opcion)
+                {
+                    case 1:
+                        
+                        prova1(nom, dataNaixement, nif, sexe, pes, alçada);
+                        break;
+                    case 2:
+                        prova2(nom, dataNaixement, nif, sexe, pes, alçada);
+                        break;
+                    case 3:
+                        prova3(nom, dataNaixement, nif, sexe, pes, alçada);
+                        break;
+                    case 4:
+                        break;
+                    default:
+                        Console.WriteLine("Opcion no valida");
+                        break;
+                }
+            } while (opcion!=4);
+            
+        }
+        private static void prova3(string nom, string dataNaixement, string nif, char sexe, double pes, double alçada)
+        {
+            Persona p3 = new Persona(nom, dataNaixement, nif, sexe, pes, alçada);
+
+            //Utilitza els mètodes CalcularIMC, Edat, EsMajorDeEdat mostrant el resultat per consola. 
+            Console.WriteLine("\nProbando Metodes");
+            Console.WriteLine("Resultado: " + p3.calcularIMC());
+            Console.WriteLine("Edad: " + p3.edat());
+            Console.WriteLine(p3.esMajorEdat() ? "Es Mayor de Edad" : "No es Mayor de Edad");
+
+            //Utilitza el mètode ToString per mostrar les dades de la persona. 
+            Console.WriteLine("\nProbando ToString");
+            Console.WriteLine(p3);
+        }
+        private static void prova2(string nom, string dataNaixement, string nif, char sexe, double pes, double alçada)
+        {
+            //Crea un objecte instanciant-lo amb el constructor que té els arguments de nom, data
+            Persona p2 = new Persona(nom, dataNaixement, sexe);
+            p2.Nif = nif;
+            p2.Pes = pes;
+            p2.Alçada = alçada;
+
+            //Utilitza els mètodes CalcularIMC, Edat, EsMajorDeEdat mostrant el resultat per consola. 
+            Console.WriteLine("\nProbando Metodes");
+            Console.WriteLine("Resultado: " + p2.calcularIMC());
+            Console.WriteLine("Edad: " + p2.edat());
+            Console.WriteLine(p2.esMajorEdat() ? "Es Mayor de Edad" : "No es Mayor de Edad");
+
+            //Utilitza el mètode ToString per mostrar les dades de la persona. 
+            Console.WriteLine("\nProbando ToString");
+            Console.WriteLine(p2);
+        }
+        private static void prova1(string nom, string dataNaixement, string nif, char sexe, double pes, double alçada)
+        {
+            //Crea un objecte instanciant-lo amb el constructor sense paràmetres i demana les dades de la
+            // persona per consola.Cal assignar-les a l’objecte mitjançant les propietats. 
+            Persona p1 = new Persona();
+            p1.Nom = nom;
+            p1.DataNaixement = dataNaixement;
+            p1.Nif = nif;
+            
+            p1.Sexe = sexe;
+            p1.Pes = pes;
+            p1.Alçada = alçada;
+
+            //Utilitza els mètodes CalcularIMC, Edat, EsMajorDeEdat mostrant el resultat per consola. 
+            Console.WriteLine("\nProbando Metodes");
+            Console.WriteLine("Resultado: " + p1.calcularIMC());
+            Console.WriteLine("Edad: " + p1.edat());
+            Console.WriteLine(p1.esMajorEdat() ? "Es Mayor de Edad" : "No es Mayor de Edad");
+
+            //Utilitza el mètode ToString per mostrar les dades de la persona. 
+            Console.WriteLine("\nProbando ToString");
+            Console.WriteLine(p1);
+        }
+        #region OtrosMetodos
+        
+        
+        
+
+
+        #endregion
+
+    }
+}
